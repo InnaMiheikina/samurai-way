4b492f6d97fc8486cb795ef7d0bbd1400a66209a
InnaMiheikina <InnaMiheikina93@gmail.com>
2022-11-12 19:14:25 +0300

    useState

---
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -0,0 +1,44 @@
+import React, {ChangeEventHandler, useState} from 'react';
+
+export type ProfileStatusType = {
+    status: string
+    updateStatus: (status: string) => void
+}
+
+const ProfileStatusWithHooks = (props: ProfileStatusType) => {
+    let [editMode, setEditMode] = useState<boolean>(false)
+    let [status, setStatus] = useState<string>(props.status)
+
+    const activateEditMode = () => {
+        setEditMode(true)
+    }
+    const deactivateEditMode = () => {
+        setEditMode(false)
+        props.updateStatus(status)
+    }
+    const onStatusChange = (e:any)=> {
+        setStatus(e.carrentTarger.value)
+    }
+
+    return (
+        <div>
+            {!editMode &&
+            <div>
+                <span onDoubleClick={activateEditMode}>{props.status}</span>
+            </div>}
+            {editMode &&
+            <div>
+                <input onChange={onStatusChange}
+                       autoFocus={true}
+                       onBlur={deactivateEditMode}
+                       value={status}
+                />
+            </div>
+            }
+        </div>
+    );
+
+};
+
+
+export default ProfileStatusWithHooks;
